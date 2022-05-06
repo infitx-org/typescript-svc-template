0f04a79a7e3a9138ac13d957879ee9558dd7f4f8
Miguel de Barros <miguel@debarros.me>
2022-05-06 14:13:13 +0200

    fix: test paths

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  clearMocks: true,
   collectCoverage: true,
   collectCoverageFrom: ['./src/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      functions: 90,
+      branches: 90,
+      lines: 90
+    }
+  },
   coverageReporters: ['json', 'lcov'],
-  clearMocks: true
 }
