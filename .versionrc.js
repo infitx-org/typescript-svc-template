195b4b988e52d2cb12d8bfaa893efe9944493743
Miguel de Barros <miguel@debarros.me>
2022-05-17 10:03:10 +0200

    chore: updates to versionrc - included ci & build types

--- .versionrc.js
+++ .versionrc.js
@@ -1,15 +1,20 @@
 /**
-* Reference: https://github.com/conventional-changelog/conventional-changelog-config-spec
+* References:
+* https://github.com/conventional-changelog/conventional-changelog-config-spec
+* https://github.com/conventional-changelog/standard-version#customizing-changelog-generation
 **/
+
 module.exports = {
-  "types": [
-    {"type": "feat", "section": "Features", "hidden": false},
-    {"type": "fix", "section": "Bug Fixes", "hidden": false},
-    {"type": "chore", "section": "Chore", "hidden": false},
-    {"type": "docs", "section": "Documentation", "hidden": false},
-    {"type": "style", "section": "Style", "hidden": false},
-    {"type": "refactor", "section": "Refactor", "hidden": false},
-    {"type": "perf", "section": "Performance", "hidden": false},
-    {"type": "test", "section": "Tests", "hidden": false}
+  types: [
+    {type: "feat", section: "Features", hidden: false},
+    {type: "fix", section: "Bug Fixes", hidden: false},
+    {type: "chore", section: "Chores", hidden: false},
+    {type: "docs", section: "Documentation", hidden: false},
+    {type: "style", section: "Style", hidden: false},
+    {type: "refactor", section: "Refactor", hidden: false},
+    {type: "perf", section: "Performance", hidden: false},
+    {type: "test", section: "Test", hidden: false},
+    {type: "ci", section: "Continuous Integration", hidden: false},
+    {type: "build", section: "Build", hidden: true}
   ]
 }
