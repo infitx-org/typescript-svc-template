47b19d554b37f9489fbb8920991e65a698f36eca
Miguel de Barros <miguel@debarros.me>
2022-05-17 09:38:42 +0200

    chore: updated .versionrc.js

--- .versionrc.js
+++ .versionrc.js
@@ -2,11 +2,11 @@ module.exports = {
   "types": [
     {"type": "feat", "section": "Features"},
     {"type": "fix", "section": "Bug Fixes"},
-    {"type": "chore", "hidden": false},
-    {"type": "docs", "hidden": false},
-    {"type": "style", "hidden": true},
-    {"type": "refactor", "hidden": false},
-    {"type": "perf", "hidden": false},
-    {"type": "test", "hidden": false}
+    {"type": "chore", "section": "Chore", "hidden": false},
+    {"type": "docs", "section": "Docs", "hidden": false},
+    {"type": "style", "section": "Style", "hidden": true},
+    {"type": "refactor", "section": "Refactor", "hidden": false},
+    {"type": "perf", "section": "Performance", "hidden": false},
+    {"type": "test", "section": "Tests", "hidden": false}
   ]
 }
